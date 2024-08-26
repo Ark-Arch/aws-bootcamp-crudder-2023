b01d68d20b088916586b3dc2226866d83ea9f4c1
David Agbemuko <davidagbemuko.ad@gmail.com>
2024-08-26 13:53:30 +0000

    added a route for notifications

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -7,6 +7,7 @@ import os
 # if the below are isolated,
 # we can literally replace this part of the code as containers or lambdas
 from services.home_activities import *
+from services.notifications_activities import *
 from services.user_activities import *
 from services.create_activity import *
 from services.create_reply import *
@@ -66,6 +67,11 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
   data = HomeActivities.run()
+  return data, 200
+
+@app.route("/api/activities/notifications", methods=['GET'])
+def data_notifications():
+  data = NotificationsActivities.run()
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
