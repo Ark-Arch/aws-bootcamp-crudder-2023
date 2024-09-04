e1928023a1eddc37c61b9d505fb635c39efbc345
David Agbemuko <davidagbemuko.ad@gmail.com>
2024-09-04 23:25:51 +0000

    initialize automatic instrumentation with Flask

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -53,6 +53,11 @@ tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
+
+# initialize automatic instrumentation with Flask
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
